4ee482d74fd1b4aa970bcfde33ca0a2019642add
Suhaani Garg <suhaanigarg2311@gmail.com>
2025-03-31 11:13:22 +0530

    third

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const handleAdd = () => {
+    setTodos([...todos, { text: input, id: Date.now() }]);
+    setInput('');
+  };
 
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Enter a todo"
+      />
+      <button onClick={handleAdd}>Add todo</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
